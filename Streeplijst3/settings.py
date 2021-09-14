c69ac1fafe94da8e8d7a42567b7514e1701779a1
s1779397 <j.j.hierck@student.utwente.nl>
2021-09-14 12:04:43 +0200

    Removed insecure key from accidental push to git

--- Streeplijst3/settings.py
+++ Streeplijst3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x)vxc1fwup^ev@)j+v9$!v5llf(q@10a5^te6!o1_#_xbq$_y&'
+SECRET_KEY = 'SuperS3cret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
