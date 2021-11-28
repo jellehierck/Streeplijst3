dd9cba175ee16aac6c2c301aa59a2ad0010ced09
s1779397 <j.j.hierck@student.utwente.nl>
2021-11-28 20:02:09 +0100

    Added post sale endpoint

--- streeplijst/urls.py
+++ streeplijst/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('products/', views.products, name='products'),
     path('products/<int:folder_id>', views.products_by_folder_id, name='products_by_folder_id'),
     path('sales/<str:username>', views.sales_by_username, name='sales_by_username'),
+    path('sales/', views.sales, name='post_sale'),
 ]
