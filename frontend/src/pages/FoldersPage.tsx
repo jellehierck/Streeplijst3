6e9dbfd58f47202fe749701c03de1343894425f0
Eli Saado <eli5saado@gmail.com>
2021-12-04 23:28:11 +0100

    Move files to pages

--- frontend/src/pages/FoldersPage.tsx
+++ frontend/src/pages/FoldersPage.tsx
@@ -1,46 +1,30 @@
 import { useEffect, useState } from "react";
-import { Card, Col, Container, Row, Spinner, Button } from "react-bootstrap";
-import { useHistory, useParams } from "react-router";
-import congresssus, { FolderType, UserType } from "./api/API";
-import { User } from "./User";
+import { Card, Col, Container, Row, Spinner } from "react-bootstrap";
+import { useHistory } from "react-router";
+import congresssus, { FolderType, UserType } from "../api/API";
+import { User } from "../User";
 
-export function FolderPage(props: any) {
+export function FoldersPage(props: any) {
   const sNumber = localStorage.getItem("sNumber");
   const [loading, setLoading] = useState(false);
-  const history = useHistory();
-  // @ts-ignore
-  let { folderId } = useParams(); // because useParams() doesn't have the correct type
-
   const [member, setMember] = useState<UserType | undefined>(undefined);
-  const [products, setProducts] = useState<any | undefined>([]);
+  const history = useHistory();
 
   useEffect(() => {
     console.log("use effect is executed :DDD", loading);
     setLoading(true);
-    Promise.all([congresssus.getMemberByUsername(sNumber || "").then((user) => {
+    congresssus.getMemberByUsername(sNumber || "").then((user) => {
       setMember(user);
-    }),
-
-    congresssus.getProductsByFolder(folderId).then((products) => {
-      setProducts(products);
-    })]).then(() => {
       setLoading(false);
-
-    })
+    });
   }, []);
-
-  let folderName = products[0]?.folder;
 
   if (loading) return <Spinner animation="border" />;
 
   return (
     <Container className="m-3">
-      <Button variant="info" onClick={() => {history.push("/folders")}}>
-          Terug
-        </Button>
       <div className="">
-        <p className="inline-block font-bold">{folderName}</p>
-        
+        <p className="inline-block font-bold">Categories</p>
         <div className="w-max mx-2 inline-block float-right">
           {loading ? (
             <Spinner animation="border"></Spinner>
@@ -50,14 +34,13 @@ export function FolderPage(props: any) {
         </div>
       </div>
       {/* <div className="mx-auto  text-center grid grid-cols-4 gap-5"> */}
-      
       <div
         className="mt-2 grid gap-3"
         style={{
           gridTemplateColumns: "repeat(auto-fill, 15rem)",
         }}
       >
-        {products.map((x: FolderType) => (
+        {props.folders.map((x: FolderType) => (
           // <div
           //   key={x.id}
           //   className="w-full p-5 justify-center flex-wrap bg-gray-100 rounded-lg"
@@ -75,6 +58,7 @@ export function FolderPage(props: any) {
             key={x.id}
             onClick={() => {
               console.log(x.id);
+              history.push(`/folders/${x.id}`);
             }}
           >
             <Card.Body>
