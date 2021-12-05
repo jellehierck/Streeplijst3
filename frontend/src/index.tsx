749a64c77c2893b471fd4babd2cb6ff68df29bd0
Eli Saado <eli5saado@gmail.com>
2021-12-06 00:11:56 +0100

    wrap app in contexts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { UserContextProvider } from "./contexts/UserContext";
+import { ShoppingCartContextProvider } from "./contexts/ShoppingCartContext";
 
 ReactDOM.render(
-    <React.StrictMode>
+  <React.StrictMode>
+    <UserContextProvider>
+      <ShoppingCartContextProvider>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+      </ShoppingCartContextProvider>
+    </UserContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
