cc31542b77842bd70046c3cc40f85b794ad181b9
s1779397 <j.j.hierck@student.utwente.nl>
2022-01-09 18:30:25 +0100

    Centered s number pad

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas, faBackspace } from "@fortawesome/free-solid-svg-icons";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faBackspace, fas} from "@fortawesome/free-solid-svg-icons";
 
-import { far, faCheckCircle } from "@fortawesome/free-regular-svg-icons";
-
-import Container from "react-bootstrap/Container";
+import {faCheckCircle, far} from "@fortawesome/free-regular-svg-icons";
 
 import SNumberPad from "./auth/SNumberPad";
-import TestAPI from "./api/TestAPI";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import congressus, { FolderType } from "./api/API";
-import { Spinner } from "react-bootstrap";
-import { FoldersPage } from "./pages/FoldersPage";
-import { FolderPage } from "./pages/FolderPage";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import congressus, {FolderType} from "./api/API";
+import {Spinner} from "react-bootstrap";
+import {FoldersPage} from "./pages/FoldersPage";
+import {FolderPage} from "./pages/FolderPage";
 
 // Initialize a font-awesome library to use icons easily throughout the project
 // src: https://fontawesome.com/v5.15/how-to-use/on-the-web/using-with/react
@@ -41,7 +38,7 @@ function App() {
   if (loading) return <Spinner animation="border" />;
 
   return (
-    <div>
+    <div className="container my-8">
       {/* <TestAPI /> */}
       <Router>
         <Switch>
--- frontend/src/auth/SNumberPad.tsx
+++ frontend/src/auth/SNumberPad.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer, useState } from "react";
+import React, {useContext, useReducer, useState} from "react";
 
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
@@ -6,12 +6,12 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 import "./SNumberPad.css";
 import congressus from "../api/API";
-import { Redirect } from "react-router";
-import { UserContext } from "../contexts/UserContext";
+import {Redirect} from "react-router";
+import {UserContext} from "../contexts/UserContext";
 
 /**
  * Possible prefixes for an SNumber.
@@ -98,8 +98,8 @@ function SNumberPad() {
           ...currState,
           prefix:
             SNumberPrefixes[
-              (indexOfCurrentPrefix + 1) % SNumberPrefixes.length
-            ],
+            (indexOfCurrentPrefix + 1) % SNumberPrefixes.length
+              ],
         };
     }
   }
@@ -113,155 +113,157 @@ function SNumberPad() {
   if (loggedIn) return <Redirect to="/folders" />;
 
   return (
-    <div className="mx-24">
-      {errored ? (
-        <div
-          className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 relative"
-          role="alert"
-        >
-          <p className="font-bold">Wrong student number</p>
-          <p>we couldn't find this student number.</p>
-          <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg
-              className="fill-current h-6 w-6 text-red-500"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
+    <div className="flex justify-center">
+      <div>
+        {errored ? (
+          <div
+            className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 relative"
+            role="alert"
+          >
+            <p className="font-bold">Wrong student number</p>
+            <p>we couldn't find this student number.</p>
+            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg
+                className="fill-current h-6 w-6 text-red-500"
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>Close</title>
+                {/*<path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />*/}
+              </svg>
+            </span>
+          </div>
+        ) : null}
+        <Row className="numpad-row">
+          <InputGroup className="mb-3">
+            <FormControl
+              value={sNumber.prefix + sNumber.sNumber}
+              aria-label="Student number input"
+              aria-describedby="s-number-input"
+            />
+            <Button
+              variant="success"
+              className="btn-sq-md"
+              onClick={() => logIn(sNumber)}
             >
-              <title>Close</title>
-              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-            </svg>
-          </span>
-        </div>
-      ) : null}
-      <Row className="numpad-row">
-        <InputGroup className="mb-3">
-          <FormControl
-            value={sNumber.prefix + sNumber.sNumber}
-            aria-label="Student number input"
-            aria-describedby="s-number-input"
-          />
-          <Button
-            variant="success"
-            className="btn-sq-md"
-            onClick={() => logIn(sNumber)}
+              <FontAwesomeIcon icon={["far", "check-circle"]} />
+            </Button>
+          </InputGroup>
+        </Row>
+        <Row className="numpad-row">
+          <ButtonGroup
+            className="btn-group-no-padding"
+            aria-label="Basic example"
           >
-            <FontAwesomeIcon icon={["far", "check-circle"]} />
-          </Button>
-        </InputGroup>
-      </Row>
-      <Row className="numpad-row">
-        <ButtonGroup
-          className="btn-group-no-padding"
-          aria-label="Basic example"
-        >
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 1 })}
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 1})}
+            >
+              <h1>1</h1>
+            </Button>
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 2})}
+            >
+              <h1>2</h1>
+            </Button>
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 3})}
+            >
+              <h1>3</h1>
+            </Button>
+          </ButtonGroup>
+        </Row>
+        <Row className="numpad-row">
+          <ButtonGroup
+            className="btn-group-no-padding"
+            aria-label="Basic example"
           >
-            <h1>1</h1>
-          </Button>
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 2 })}
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 4})}
+            >
+              <h1>4</h1>
+            </Button>
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 5})}
+            >
+              <h1>5</h1>
+            </Button>
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 6})}
+            >
+              <h1>6</h1>
+            </Button>
+          </ButtonGroup>
+        </Row>
+        <Row className="numpad-row">
+          <ButtonGroup
+            className="btn-group-no-padding"
+            aria-label="Basic example"
           >
-            <h1>2</h1>
-          </Button>
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 3 })}
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 7})}
+            >
+              <h1>7</h1>
+            </Button>
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 8})}
+            >
+              <h1>8</h1>
+            </Button>
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 9})}
+            >
+              <h1>9</h1>
+            </Button>
+          </ButtonGroup>
+        </Row>
+        <Row className="numpad-row">
+          <ButtonGroup
+            className="btn-group-no-padding"
+            aria-label="Basic example"
           >
-            <h1>3</h1>
-          </Button>
-        </ButtonGroup>
-      </Row>
-      <Row className="numpad-row">
-        <ButtonGroup
-          className="btn-group-no-padding"
-          aria-label="Basic example"
-        >
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 4 })}
-          >
-            <h1>4</h1>
-          </Button>
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 5 })}
-          >
-            <h1>5</h1>
-          </Button>
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 6 })}
-          >
-            <h1>6</h1>
-          </Button>
-        </ButtonGroup>
-      </Row>
-      <Row className="numpad-row">
-        <ButtonGroup
-          className="btn-group-no-padding"
-          aria-label="Basic example"
-        >
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 7 })}
-          >
-            <h1>7</h1>
-          </Button>
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 8 })}
-          >
-            <h1>8</h1>
-          </Button>
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 9 })}
-          >
-            <h1>9</h1>
-          </Button>
-        </ButtonGroup>
-      </Row>
-      <Row className="numpad-row">
-        <ButtonGroup
-          className="btn-group-no-padding"
-          aria-label="Basic example"
-        >
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "togglePrefix" })}
-          >
-            {SNumberPrefixes.join("/")}
-          </Button>
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "add", nr: 0 })}
-          >
-            <h1>0</h1>
-          </Button>
-          <Button
-            variant="secondary"
-            className="btn-sq-md"
-            onClick={() => sNumberDispatch({ type: "remove" })}
-          >
-            <FontAwesomeIcon icon={["fas", "backspace"]} />
-          </Button>
-        </ButtonGroup>
-      </Row>
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "togglePrefix"})}
+            >
+              {SNumberPrefixes.join("/")}
+            </Button>
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "add", nr: 0})}
+            >
+              <h1>0</h1>
+            </Button>
+            <Button
+              variant="secondary"
+              className="btn-sq-md"
+              onClick={() => sNumberDispatch({type: "remove"})}
+            >
+              <FontAwesomeIcon icon={["fas", "backspace"]} />
+            </Button>
+          </ButtonGroup>
+        </Row>
+      </div>
     </div>
   );
 }
