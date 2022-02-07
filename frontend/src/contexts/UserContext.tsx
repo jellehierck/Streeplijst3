0b2f76ab11e8870be6088ba4f7a895f6c4e7faa8
s1779397 <j.j.hierck@student.utwente.nl>
2022-02-07 20:07:16 +0100

    Updated UserContext to be simpler

--- frontend/src/contexts/UserContext.tsx
+++ frontend/src/contexts/UserContext.tsx
@@ -1,33 +1,64 @@
-import React, {createContext, useEffect, useState} from "react";
-import {Spinner} from "react-bootstrap";
-import congresssus, {UserType} from "../api/API";
+import React, { createContext, Dispatch, useReducer } from "react";
+import { MemberType } from "../api/API";
 
-export const UserContext = createContext<UserType | any>([]);
+export const UserContext = createContext<MemberType | null>(null);
 
-export function UserContextProvider(props: any) {
-  const sNumber = localStorage.getItem("sNumber");
-
-  const [user, setUser] = useState<UserType | undefined | null>();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    console.log("user useeffect is ebexj :XXX");
-    if (sNumber) {
-      // only if loggedIn
-      congresssus.getMemberByUsername(sNumber || "").then((user) => {
-        setUser(user);
-        setLoading(false);
-      });
-    } else {
-      setLoading(false);
-    }
-  }, []);
-
-  if (loading) return <Spinner animation="grow"></Spinner>;
-
-  return (
-    <UserContext.Provider value={[user, setUser]}>
-      {props.children}
-    </UserContext.Provider>
-  );
+// Possible actions to take on the user
+const enum UserAction {
+  LOGIN = "LOGIN",
+  LOGOUT = "LOGOUT"
 }
+
+// Grouping of user action
+type UserActionType = {
+  type : UserAction
+  payload? : UserStateType
+}
+
+// User state type, contains member information
+type UserStateType = {
+  member? : MemberType
+}
+
+// Initial user state
+const initialUserState : UserStateType = {member: undefined};
+
+/**
+ * Reducer function for the user state
+ * @param state Current user state
+ * @param action Action to take on the user state
+ */
+const userReducerFunc = (state : UserStateType, action : UserActionType) : UserStateType => {
+  const {type, payload} = action;  // Unpack the action
+
+  // Determine how to adjust the state according to the action type
+  switch (type) {
+    case UserAction.LOGIN:
+      return {...state, ...payload};  // Set the user as logged in
+    case UserAction.LOGOUT:
+      return initialUserState;  // Set no user as logged in
+  }
+};
+
+// Context to pass along
+type UserContextType = {
+  state : UserStateType
+  dispatch : Dispatch<UserActionType>
+}
+
+// Store to keep track of user state, basically the storage of the context
+const UserStore = createContext<UserContextType>({} as UserContextType);
+
+// User context provider
+const UserContextProvider : React.FC = (props) => {
+  const [state, dispatch] = useReducer<React.Reducer<UserStateType, UserActionType>>(userReducerFunc, initialUserState);
+
+  return <UserStore.Provider value={{state, dispatch}}>
+    {props.children}
+  </UserStore.Provider>
+}
+
+// Exports
+export default UserContextProvider;
+export type { UserStateType, UserActionType, UserContextType };
+export { UserAction, };
