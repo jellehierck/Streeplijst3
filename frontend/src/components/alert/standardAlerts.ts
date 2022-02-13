bf773473d7051f26999346a5c0c9aeb08dadd09e
s1779397 <j.j.hierck@student.utwente.nl>
2022-02-13 18:08:43 +0100

    Added docstring

--- frontend/src/components/alert/standardAlerts.ts
+++ frontend/src/components/alert/standardAlerts.ts
@@ -1,10 +1,16 @@
 import { AlertStateType } from "./AlertContext";
 
-const usernameNotFoundAlert = (username : string) : AlertStateType => {
+/**
+ * Displays a message if a user is not found
+ * @param {string} username Username which is not found
+ * @param {string | undefined} rawError Raw error message
+ * @returns {AlertStateType}
+ */
+const usernameNotFoundAlert = (username : string, rawError? : string) : AlertStateType => {
   return {
     display: {
       heading: `${username} niet gevonden`,
-      message: `Probeer het nog een keer`,
+      message: `Probeer het nog een keer. ${rawError}`,
       variant: "warning",
     },
     timeout: 10000,  // 10 seconds
