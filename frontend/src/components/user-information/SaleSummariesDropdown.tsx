c80f40d49a312a2a680a4991da67eedb453b38f4
s1779397 <j.j.hierck@student.utwente.nl>
2022-04-01 17:27:38 +0200

    Formatted sale accordion item header

--- frontend/src/components/user-information/SaleSummariesDropdown.tsx
+++ frontend/src/components/user-information/SaleSummariesDropdown.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Accordion } from "react-bootstrap";
+import { Accordion, ButtonGroup, ButtonToolbar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import { SaleSummaryType } from "../../api/QueryTestComponent";
 import ItemCardGrid from "../products/ItemCardGrid";
 import SaleSummaryCard from "./SaleSummaryCard";
@@ -9,18 +10,54 @@ type SaleSummariesDropdownProps = {
 }
 
 // React component
-const SaleSummariesDropdown : React.FC<SaleSummariesDropdownProps> = (props) => {
+const SaleSummariesDropdown : React.FC<SaleSummariesDropdownProps> = ({
+  saleSummaries,
+}) => {
 
-  const saleAccordionItems = props.saleSummaries.map(saleSummary => {
+  const saleAccordionItems = saleSummaries.map(saleSummary => {
     // Create a card per product sold to be displayed inside the accordion
     const productCards = saleSummary.products.map(productSummary => {
       return <SaleSummaryCard productSummary={productSummary}
                               key={productSummary.sale_invoice_id} />;
     });
 
+    // Convert date to a dutch human readable format
+    const saleDate = new Date(saleSummary.date);
+    const saleDateString = saleDate.toLocaleDateString("nl-NL", {dateStyle: "long"});
+
+    // Count total number of items bought
+    const totalQuantity = saleSummary.products.reduce((totalQuantity, product) => {
+      return totalQuantity + product.quantity;
+    }, 0);
+    const totalQuantityString = () => {
+      if (totalQuantity === 1) {
+        return `${totalQuantity} item`;
+      } else {
+        return `${totalQuantity} items`;
+      }
+    };
+
     // Create an accordion to have a stack of dropdown menus per sale invoice
     return <Accordion.Item eventKey={saleSummary.id.toString()}>
-      <Accordion.Header>{saleSummary.date} - €{saleSummary.total_price}</Accordion.Header>
+      <Accordion.Header>
+        <ButtonToolbar className="w-50">
+          <Button variant="secondary"
+                  className="w-50 p-1 text-start"
+                  disabled>
+            {saleDateString}
+          </Button>
+          <Button variant="secondary"
+                  className="w-25 p-1 text-start"
+                  disabled>
+            {totalQuantityString()}
+          </Button>
+          <Button variant="secondary"
+                  className="w-25 p-1 text-end"
+                  disabled>
+            Total: €{saleSummary.total_price.toFixed(2)}
+          </Button>
+        </ButtonToolbar>
+      </Accordion.Header>
       <Accordion.Body>
         <ItemCardGrid cards={productCards} xl={5} lg={4} md={3} sm={2} />
       </Accordion.Body>
