603a84c46cc39b531a9ee00aecb837e9dd610526
s1779397 <j.j.hierck@student.utwente.nl>
2022-09-01 10:58:53 +0200

    Initial commit

--- Streeplijst3/settings.py
+++ Streeplijst3/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'streeplijst.apps.StreeplijstConfig',
+    'nfc_reader.apps.NfcReaderConfig',
     'frontend',
     'corsheaders'
 ]
---
+++ nfc_reader/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NfcReaderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nfc_reader'
