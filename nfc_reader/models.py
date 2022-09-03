0cfd858dbdd0f192d06ffe617eb44555212a7533
s1779397 <j.j.hierck@student.utwente.nl>
2022-09-03 20:46:37 +0200

    Added UserNfcCard database model

--- nfc_reader/models.py
+++ nfc_reader/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class UserNfcCard(models.Model):
+    """Connection between a user and a NFC card"""
+    user_id = models.IntegerField()  # User id as stored in Congressus
+    card_uid = models.CharField(max_length=20, unique=True)  # Card UID stored as hex, e.g. "00 00 00 00 00 00 00"
+    added = models.DateTimeField(auto_now_add=True)  # When this card was added, set automatically
