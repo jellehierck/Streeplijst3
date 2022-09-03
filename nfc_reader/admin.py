15c6da458db9fe064907d7ceab47bb4e559e2121
s1779397 <j.j.hierck@student.utwente.nl>
2022-09-03 21:28:50 +0200

    Added admin view to UserNfcCard model

--- nfc_reader/admin.py
+++ nfc_reader/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from nfc_reader.models import UserNfcCard
+
+admin.site.register(UserNfcCard)
--- nfc_reader/models.py
+++ nfc_reader/models.py
@@ -4,6 +4,9 @@ from django.db import models
 # Create your models here.
 class UserNfcCard(models.Model):
     """Connection between a user and a NFC card"""
-    user_id = models.IntegerField()  # User id as stored in Congressus
+    username = models.CharField(max_length=8, primary_key=True)  # Username as stored in Congressus
     card_uid = models.CharField(max_length=20, unique=True)  # Card UID stored as hex, e.g. "00 00 00 00 00 00 00"
     added = models.DateTimeField(auto_now_add=True)  # When this card was added, set automatically
+
+    def __str__(self) -> str:
+        return f"<{self.__class__.__name__} {self.username} ({self.added.strftime('%Y-%m-%d %H:%M:%S')})>"
