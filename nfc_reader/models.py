7f0cb32e6dfd1c4576f6fea99a214602a073559d
s1779397 <j.j.hierck@student.utwente.nl>
2022-09-05 14:00:48 +0200

    CurrentlyConnectedCard renamed to LastConnectedCard to store which card was last connected (also if card is removed)

--- nfc_reader/models.py
+++ nfc_reader/models.py
@@ -12,15 +12,16 @@ class UserNfcCard(models.Model):
         return f"<{self.__class__.__name__} {self.username} ({self.added.strftime('%Y-%m-%d %H:%M:%S')})>"
 
 
-class CurrentlyConnectedCard(models.Model):
+class LastConnectedCard(models.Model):
     """
-    Database table which holds only a single row, to keep track of whether a card is currently connected to the reader.
+    Database table which holds only a single row, to keep track of the last card connected to the reader.
     NB: This is likely not a very sensible approach but it is guaranteed thread-safe.
     """
     card_uid = models.CharField(max_length=20)  # Card UID stored as hex, e.g. "00 00 00 00 00 00 00"
-    updated = models.DateTimeField(auto_now=True)  # When this card was connected
+    currently_connected = models.BooleanField(default=True)  # Whether the card is currently connected
+    connected = models.DateTimeField(auto_now_add=True)  # When this card was connected
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) -> None:
         """
         Override the save method so that only one row can be present at any time.
         Based on: https://stackoverflow.com/a/60274685
@@ -28,5 +29,13 @@ class CurrentlyConnectedCard(models.Model):
         self.pk = 1  # Ensures every saved card always overwrites any previous card due to having the same primary key
         super().save(*args, **kwargs)
 
+    def delete(self, using=None, keep_parents=False) -> None:
+        """
+        Override the delete method so that it never deletes an entry in the list but instead sets the
+        currently_connected flag to False.
+        """
+        self.currently_connected = False
+
     def __str__(self) -> str:
-        return f"<{self.__class__.__name__} {self.card_uid}>"
+        connected_str = "connected" if self.currently_connected else "not connected"
+        return f"<{self.__class__.__name__} {self.card_uid} ({connected_str})>"
--- nfc_reader/nfc.py
+++ nfc_reader/nfc.py
@@ -7,35 +7,42 @@ from smartcard.CardConnection import CardConnection
 from smartcard.CardMonitoring import CardObserver, CardMonitor
 from smartcard.util import toHexString
 
-from nfc_reader.models import CurrentlyConnectedCard
+from nfc_reader.models import LastConnectedCard
 
 GET_UID = [0xFF, 0xCA, 0x00, 0x00, 0x00]
 """Command to get card uid"""
 
 
-class CurrentlyConnectedCardObserver(CardObserver):
+class LastConnectedCardObserver(CardObserver):
 
     def __init__(self):
-        """Observe insertion/removal of cards and store the currently connected card in the database"""
+        """Observe insertion/removal of cards and store the last connected card in the database"""
         super().__init__()
         self.currently_connected_card_uid: Optional[str] = None
 
     def update(self, observable, handlers: tuple[list[Card], list[Card]]) -> None:
         cards_added, cards_removed = handlers
-        for card in cards_added:
-            print(f"+Inserted: {toHexString(card.atr)}")
+        for card in cards_added:  # This block only runs when cards are connected
+            # Create connection to card
             card_connection: CardConnection = card.createConnection()
             card_connection.connect()
+
+            # Get UID from card
             response, sw1, sw2 = card_connection.transmit(GET_UID)
             self.currently_connected_card_uid = toHexString(response)
-            currently_connected_card = CurrentlyConnectedCard(card_uid=self.currently_connected_card_uid)
-            currently_connected_card.save()
-            print(f"uid: {currently_connected_card}")
+            print(f"+Inserted: {self.currently_connected_card_uid}")  # TODO: replace with logging
 
-        for card in cards_removed:
-            print(f"- Removed: {toHexString(card.atr)}")
-            # Remove from database
-            print(f"uid: {self.currently_connected_card_uid}")
+            # Add to database
+            currently_connected_card = LastConnectedCard(card_uid=self.currently_connected_card_uid)
+            currently_connected_card.save()
+
+        for card in cards_removed:  # This block only runs when cards are removed
+            last_connected_cards = LastConnectedCard.objects.all()  # Get all cards (should be 1 or 0)
+            for last_card in last_connected_cards:  # Delete all cards
+                # (does not actually delete but sets currently_connected to false)
+                last_card.delete()
+
+            print(f"- Removed: {self.currently_connected_card_uid}")  # TODO: replace with logging
             self.currently_connected_card_uid = None
 
 
@@ -43,7 +50,7 @@ class Acr122uWaitingThread(Thread):
     def __init__(self) -> None:
         super().__init__(name="ACR122U", daemon=True)
         self.card_monitor = CardMonitor()
-        self.card_observer = CurrentlyConnectedCardObserver()
+        self.card_observer = LastConnectedCardObserver()
 
     def run(self) -> None:
         print(f"Starting {self.name}")  # TODO: Replace with logging
@@ -51,4 +58,4 @@ class Acr122uWaitingThread(Thread):
         self.card_monitor.addObserver(self.card_observer)  # Start observing card insertion/removal
 
         while True:  # Loop forever
-            sleep(10)  # Sleep for some time since this thread does nothing anymore
+            sleep(10)  # Sleep for some time since this thread does nothing anymore, uses almost no CPU this way
